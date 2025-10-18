9a17974bc820bc625558fa11676ff740341cc773
Aryan Manhas <aryanmanhas06@gmail.com>
2025-10-18 18:24:40 +0530

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -1,64 +1,30 @@
-import os
-from langchain_community.document_loaders import (
-    DirectoryLoader,
-    PyPDFLoader,
-    TextLoader,
-    Docx2txtLoader,
-)
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceInstructEmbeddings
-from langchain_community.vectorstores import FAISS
+import glob
+from PyPDF2 import PdfReader
 
-data_path = "data/"
-faiss_db = "vectorstores/db_faiss"
+"""
+This function takes all the pdf available in the data folder and extract the information from them.
+Also provide metadata such as Author, Subject and Title
+"""
 
 
-def create_documents():
-
-    print("Loading documents...")
-    loader = DirectoryLoader(
-        data_path,
-        glob="**/*",
-        use_multithreading=True,
-        show_progress=True,
-    )
-
-    documents = loader.load()
-    print(f"Loaded {len(documents)} documents.")
-    return documents
-
-
-def split_text(documents):
-    print("Splitting documents into smaller chunks...")
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=100,
-    )
-    texts = text_splitter.split_documents(documents)
-    print(f"Split into {len(texts)} chunks.")
-    return texts
+def pdfreader(file_name):
+    for file in file_name:
+        print(f"Accessing information from: {file} \n")
+        reader = PdfReader(file)
+        page_count = len(reader.pages)
+        print("Extracted Information from the Page is:\n")
+        for i in range(page_count):
+            page = reader.pages[i]
+            print("Page No.", i + 1)
+            print(page.extract_text())
+        meta = reader.metadata
+        print("Meta Data related to the PDF is:\n ")
+        print(f"Author: {meta.author}")
+        print(f"Subject:  {meta.subject}")
+        print(f"Title: \n\n{meta.title}")
 
 
-def create_vector_store(texts):
+file_list_pdf = glob.glob("data/*.pdf")
 
-    print("Creating embeddings and vector store...")
-
-    embedding_model = HuggingFaceInstructEmbeddings(
-        model_name="hkunlp/instructor-large",
-        model_kwargs={"device": "cpu"},
-    )
-
-    db = FAISS.from_documents(texts, embedding_model)
-
-    db.save_local(faiss_db)
-    print(f"Vector store created and saved at {faiss_db}")
-    return db
-
-
-if __name__ == "__main__":
-
-    docs = create_documents()
-
-    chunks = split_text(docs)
-
-    create_vector_store(chunks)
+if len(file_list_pdf) != 0:
+    pdfreader(file_list_pdf)
