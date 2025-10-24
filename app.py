68724f7a210990103970781d0f1cbc9c42a6d6f6
Aryan Manhas <aryanmanhas06@gmail.com>
2025-10-25 02:27:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 import os
-import streamlit
+import streamlit as st
 from langchain_community.vectorstores import FAISS
+
+
+st.set_page_config(
+    page_title="AI Academic Assistant",
+    page_icon="🤖",
+)
+st.header("Hello, User!")
+st.title("AI Academic Assistant📚")
+query = st.text_input("Ask AI")
