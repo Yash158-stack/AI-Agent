7f8d24fb62623ad317965c3cc3cb60b462515cc3
Aryan Manhas <aryanmanhas06@gmail.com>
2025-10-26 00:51:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,28 @@ st.set_page_config(
 st.header("Hello, User!")
 st.title("AI Academic Assistant📚")
 query = st.text_input("Ask AI")
+
+
+@st.cache_resource
+def load_retriever():
+    if not os.path.isdir("faiss_db"):
+        st.error(
+            "Possible reasons for the error:\n"
+            "1. The ingest.py script has not been executed.\n"
+            "2. No PDF or DOCX files were found in the 'data' subfolders.\n"
+            "> Solution : Run ingest.py or Add your PDFs and DOCXs files for reference.",
+            icon="🚨",
+        )
+        return None
+
+    embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    db = FAISS.load_local(
+        folder_path="faiss_db",
+        embeddings=embedding,
+        allow_dangerous_deserialization=True,
+    )
+
+    retriever = db.as_retriever(search_kwargs={"k": 4})
+
+    return retriever
